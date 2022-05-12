56000e1ecc26011e99544fcf919a78266ef70767
nevol78@mail.ru <nevol78@mail.ru>
2022-05-12 22:10:26 +0300

    Task6

---
+++ task6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите целое число ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Число чётное? ");
+if (num % 2 == 0) Console.Write("да");
+else Console.Write("нет");
+
