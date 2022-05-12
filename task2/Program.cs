557aeeaf497f31e4fc524649cb0ea90999b03065
nevol78@mail.ru <nevol78@mail.ru>
2022-05-12 21:50:33 +0300

    Task2

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число 1 -> ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2 -> ");
+int num2 = int.Parse(Console.ReadLine());
+int max = num1;
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+Console.Write("max = ");
+Console.WriteLine(max);
